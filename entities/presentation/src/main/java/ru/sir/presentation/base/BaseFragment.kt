7d1bdc38d66b9529c7b16a68b28f945ce1746228
SIR <henkok398@mail.ru>
2021-03-05 16:12:06 +0300

    In BaseFragment view binding support added

--- entities/presentation/src/main/java/ru/sir/presentation/base/BaseFragment.kt
+++ entities/presentation/src/main/java/ru/sir/presentation/base/BaseFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.viewbinding.ViewBinding
 import javax.inject.Inject
 
-abstract class BaseFragment<T : BaseViewModel, B : ViewDataBinding>(private val type: Class<out T>) : Fragment() {
+abstract class BaseFragment<T : BaseViewModel, B : ViewBinding>(private val type: Class<out T>) : Fragment() {
     protected lateinit var viewModel: T
     protected lateinit var binding: B
 
